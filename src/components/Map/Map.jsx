88691b076755ac360063908a4b4af1dbc27c9fd4
abimbolataofeek <abimbolaadewale9999@gmail.com>
2022-01-20 09:13:02 +0100

    still working on map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOutlined from '@material-ui/icons/LocationOnOutlined';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
@@ -10,13 +10,13 @@ const Map = () => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
 
-    const corrdinates = { lat: 0, lng: 0 };
+    const coordinates = { lat: 0, lng: 0 };
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
-                bootsrapURLKeys={{ key: 'AIzaSyCChF5lTF4RwVSFK4au2ctlFEe9LtwgWtE' }}
-                defaultCenter={corrdinates}
-                center={corrdinates}
+                bootstrapURLKeys={{ key: '' }}
+                defaultCenter={coordinates}
+                center={coordinates}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
                 option={''}
